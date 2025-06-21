ec3a80d9af1ebe70a843cd63b49cc4b93220df00
Rutab16a <owaiskhan031245@gmail.com>
2025-06-21 12:53:44 +0500

    Create payroll.dart

---
+++ payroll.dart
@@ -0,0 +1,37 @@
+void main() {
+  List<Employee> employees = [Employee('r161', 'Rutab', 50000, 10, 2000)];
+
+  print("Payroll Management Of Employees");
+  print("--------------------------------");
+
+  for (Employee employee in employees) {
+    double tax = employee.calculateTax();
+    double NetSalary = employee.calculateNetSalary();
+    print("Employee ID: {r161}");
+    print("Name: {Rutab}");
+    print("Gross Salary: {50000}");
+    print("Tax: $tax");
+    print("Benefits: {2000}");
+    print("Net Salary: $NetSalary");
+    print("---------------------------------");
+  }
+}
+
+class Employee {
+  String id;
+  String name;
+  double salary;
+  double taxRate;
+  double benefits;
+
+  Employee(this.id, this.name, this.salary, this.taxRate, this.benefits);
+
+  double calculateTax() {
+    return salary * taxRate / 100;
+  }
+
+  double calculateNetSalary() {
+    double tax = calculateTax();
+    return salary - taxRate / 100;
+  }
+}
